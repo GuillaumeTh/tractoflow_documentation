633eb888b4cbd4222720dc1aaa8b9e557890e727
Guillaume Theaud <guillaume.theaud@gmail.com>
2020-06-29 09:59:09 -0400

    Add quiet options

--- source/data/install.sh
+++ source/data/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-wget https://github.com/scilus/tractoflow/releases/download/2.1.0/tractoflow-2.1.0.zip
-unzip tractoflow-2.1.0.zip
-rm tractoflow-2.1.0.zip
-wget http://scil.usherbrooke.ca/containers_list/tractoflow_2.1.0_feb64b9_2020-05-29.img
+echo "Downloading TractoFlow..."
+wget –quiet https://github.com/scilus/tractoflow/releases/download/2.1.0/tractoflow-2.1.0.zip
+unzip -q tractoflow-2.1.0.zip
+rm -rf tractoflow-2.1.0.zip
+wget –quiet http://scil.usherbrooke.ca/containers_list/tractoflow_2.1.0_feb64b9_2020-05-29.img
+echo "Done: TractoFlow downloaded."
