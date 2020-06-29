fd88cbe783a493a0a9baa6f64862b4b721840fe6
Guillaume Theaud <guillaume.theaud@gmail.com>
2020-06-29 09:54:59 -0400

    First version of script

---
+++ source/data/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://github.com/scilus/tractoflow/releases/download/2.1.0/tractoflow-2.1.0.zip
+unzip tractoflow-2.1.0.zip
+rm tractoflow-2.1.0.zip
+wget http://scil.usherbrooke.ca/containers_list/tractoflow_2.1.0_feb64b9_2020-05-29.img
